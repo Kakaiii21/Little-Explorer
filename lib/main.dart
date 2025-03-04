639f3659d374ff626104bdbff09b5b18202f2084
Kakaiii21 <dellamascasey@gmail.com>
2025-03-04 19:31:22 +0800

    Home Screen Front-End Done

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: LoginScreen(),git checkout main
+
     );
   }
 }
